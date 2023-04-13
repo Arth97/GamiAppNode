b0907c97e7d3158042afcac4f20422b8e4cb9597
Arturo <arturotinajas@gmail.com>
2023-04-14 01:08:03 +0200

    challenge changes

--- src/application/challengeUseCase.ts
+++ src/application/challengeUseCase.ts
@@ -5,10 +5,36 @@ export class ChallengeUseCase {
   
   constructor(private readonly _challengeRepository: IChallengeRepository) { }
 
+  public async getChallengeById(): Promise<IChallengeEntity> {
+    try {
+      const challenge = this._challengeRepository.getChallengeById(/*Parameters TBD*/)
+      return challenge
+    } catch (err) {
+      throw err
+    }
+  }
+
   public async createChallenge(): Promise<IChallengeEntity> {
     try {
       const createdChallenge = this._challengeRepository.createChallenge(/*Parameters TBD*/)
       return createdChallenge
+    } catch (err) {
+      throw err
+    }
+  }
+
+  public async updateChallenge(): Promise<IChallengeEntity> {
+    try {
+      const updatedChallenge = this._challengeRepository.updateChallenge(/*Parameters TBD*/)
+      return updatedChallenge
+    } catch (err) {
+      throw err
+    }
+  }
+
+  public async deleteChallenge(): Promise<Boolean> {
+    try {
+      return this._challengeRepository.deleteChallenge(/*Parameters TBD*/)
     } catch (err) {
       throw err
     }
--- src/domain/challenge/challengeRepository.ts
+++ src/domain/challenge/challengeRepository.ts
@@ -1,5 +1,8 @@
 import { IChallengeEntity } from "./challengeEntity";
 
 export interface IChallengeRepository {
+  getChallengeById(): Promise<IChallengeEntity>
   createChallenge(): Promise<IChallengeEntity>
+  updateChallenge(): Promise<IChallengeEntity>
+  deleteChallenge(): Promise<Boolean>
 }
