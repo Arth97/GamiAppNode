1752124d8e5a036b1ce31d69c0dcb20b29c224e9
Arturo <arturotinajas@gmail.com>
2023-04-15 01:19:49 +0200

    Mongo connection

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 
+import userRoutes from './infrastructure/routes/userRoutes'
+import dbInit from './infrastructure/DB/mongo'
+
 dotenv.config()
 
-import userRoutes from './infrastructure/routes/userRoutes'
-
+const PORT = process.env.PORT || 3001
 const app = express()
 
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => {
-  // console.log(process.env)
-  console.log('🚀Listening on port:', PORT)
-})
+app.use(express.json())
 
 app.use('/api/users', userRoutes)
+// app.use('/api/games', gameRoutes)
+
+dbInit();
+app.listen(PORT, () => {
+  console.log('🚀Listening on port:', PORT)
+})
---
+++ src/infrastructure/DB/mongo.ts
@@ -0,0 +1,16 @@
+import { connect } from "mongoose";
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const MONGO_URI = process.env.MONGO_URI
+
+const dbInit = () => {
+  connect(MONGO_URI).then(() => {
+    console.log("Mongo DataBase connected")
+  }).catch(err => {
+    console.error("Err", err)
+  })
+}
+
+export default dbInit
