829251f730d202bf25944213999ece6c94c42952
Arturo <arturotinajas@gmail.com>
2023-04-19 14:22:19 +0200

    user cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
+import cors from 'cors'
 
 import userRoutes from './infrastructure/routes/userRoutes'
 import dbInit from './infrastructure/DB/mongo'
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/users', userRoutes)
 // app.use('/api/games', gameRoutes)
